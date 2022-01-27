4a50cb144b48c9bc6875c1ec307a3fc8ae03de21
Joeyhobs_lp <33493957+joeyhobs@users.noreply.github.com>
2022-01-27 16:50:22 +0000

    All changes commited and ready for hosting

---
+++ .eleventy.js
@@ -0,0 +1,24 @@
+const { DateTime } = require("luxon"); /*we are loading in luxon 
+    whichi is a date formating module ane we will use that to 
+    format the date.
+    */
+
+module.exports = function (eleventyConfig) {
+    // we want eleventy to grab the styles and assets folders as well
+    eleventyConfig.addPassthroughCopy("./src/style.css");
+    eleventyConfig.addPassthroughCopy("./src/assets");
+
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).
+        toLocaleString(DateTime.DATE_MED);
+    });
+    /** 
+        declare where the src files (input) and the public facingfiles (output) will be.     
+    **/
+    return {
+        dir: {
+            input: "src", //this tells eleventy where your input folder is
+            output: "Public", //this tells eleventy where it should ouput the build files.
+        }, //If not declared it will default to _site
+    };
+};
